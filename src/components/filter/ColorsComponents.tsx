ac7acfa72d2bfbb04221e8849e80ffc0891daacd
Shamimusman515419 <shamimhossain01617@gmail.com>
2024-02-29 13:59:31 +0600

    size  select

--- src/components/filter/ColorsComponents.tsx
+++ src/components/filter/ColorsComponents.tsx
@@ -3,38 +3,37 @@ import { ColorInterface, Colors } from "@/utility/filterItem";
 import { useDispatch, useSelector } from "react-redux";
 
 const ColorsComponents = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const { colors } = useSelector((state: any) => state.Colors);
+    const { colors } = useSelector((state: any) => state.Colors);
 
-  return (
-    <div>
-      <div className="flex items-center flex-wrap gap-7 ">
-        {Colors?.map((color: ColorInterface, i: number) => {
-          const isColor = colors.find((item: number) => item == color.id);
+    return (
+        <div>
+            <div className="flex items-center flex-wrap gap-7 ">
+                {Colors?.map((color: ColorInterface, i: number) => {
+                    const isColor = colors.find((item: number) => item == color.id);
 
-          return (
-            <div
-              onClick={() => dispatch(AddColor(color?.id))}
-              key={i}
-              className=" group"
-            >
-              <div
-                className={` ${
-                  isColor && "border"
-                } group-hover:border  w-[70px]  flex justify-center items-center   h-[70px]  border-[#000000e4] rounded-xl `}
-              >
-                <div
-                  className={` w-[70px] group-hover:h-[60px]  group-hover:w-[60px] duration-300 ${color?.color_code}   h-[70px]   rounded-xl cursor-pointer transform `}
-                ></div>
-              </div>
-              <h1 className="text-center text-[18px] py-1">{color?.name}</h1>
+                    return (
+                        <div
+                            onClick={() => dispatch(AddColor(color?.id))}
+                            key={i}
+                            className=" group"
+                        >
+                            <div
+                                className={` ${isColor && "border"
+                                    } group-hover:border  w-[70px]  flex justify-center items-center   h-[70px]  border-[#000000e4] rounded-xl `}
+                            >
+                                <div
+                                    className={` ${isColor ? "w-[60px]  h-[60px]" : " w-[70px]  h-[70px] "} group-hover:h-[60px]  group-hover:w-[60px] duration-300 ${color?.color_code}     rounded-xl cursor-pointer transform `}
+                                ></div>
+                            </div>
+                            <h1 className="text-center text-[18px] py-1">{color?.name}</h1>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default ColorsComponents;
