d1137492d9f8bb740bb08faf055d480bcbd5c36d
Shamimusman515419 <shamimhossain01617@gmail.com>
2024-03-27 09:59:12 +0600

    search resolve

--- src/components/showProduct/ShowProduct.tsx
+++ src/components/showProduct/ShowProduct.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const ShowProduct = () => {
+const ShowProduct = ({ products }: any) => {
     return (
         <>
             <div className=' flex justify-between items-center py-10  border-y border-y-white  mt-10 text-white '>
-                <p>Products (9)</p>
-                <p>Showing 9 results</p>
+                <p>Products ({products?.length})</p>
+                <p>Showing {products?.length} results</p>
             </div>
         </>
     );
