2c53d2c98d0c6df5d29f539e2355ebf4a5aaaf99
MD Masud Rana <66163632+mdmasudranainfo@users.noreply.github.com>
2024-03-31 15:24:57 +0600

    asd

--- src/components/productDetail/ProductReviews/AddReview.tsx
+++ src/components/productDetail/ProductReviews/AddReview.tsx
@@ -39,6 +39,7 @@ const AddReview = ({ product_id }: any) => {
   };
   useEffect(() => {
     if (createReviewResult && isSuccess) {
+      console.log(createReviewResult);
       toast.success(createReviewResult?.message);
     }
   }, [isSuccess, createReviewResult]);
