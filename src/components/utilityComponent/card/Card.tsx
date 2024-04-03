ad3274fc183ec7aa3b07d2510e83e3bd80db9da3
MD Masud Rana <66163632+mdmasudranainfo@users.noreply.github.com>
2024-04-03 09:02:02 +0600

    video

--- src/components/utilityComponent/card/Card.tsx
+++ src/components/utilityComponent/card/Card.tsx
@@ -1,14 +1,15 @@
-'use client';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
-import Button from '../button/Button';
-import { GrFavorite } from 'react-icons/gr';
-import Link from 'next/link';
-import { useDispatch, useSelector } from 'react-redux';
-import { addItem } from '@/redux/features/cart/CartSlice';
-import { addFavItem } from '@/redux/features/favorite/favoriteSlice';
-import { ProductInterface } from '@/types/Productinterface';
-import VideoPlayer from '@/components/videoPlayer/VideoPlayer';
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import Button from "../button/Button";
+import { GrFavorite } from "react-icons/gr";
+import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { addItem } from "@/redux/features/cart/CartSlice";
+import { addFavItem } from "@/redux/features/favorite/favoriteSlice";
+import { ProductInterface } from "@/types/Productinterface";
+import VideoPlayer from "@/components/videoPlayer/VideoPlayer";
+import ReactPlayer from "react-player/lazy";
 
 const Card = ({ button, fav, product }: any) => {
   const dispatch = useDispatch();
@@ -20,8 +21,10 @@ const Card = ({ button, fav, product }: any) => {
 
   let photos = [];
   if (product?.photos) {
-    photos.push(...product.photos.split(','));
+    photos.push(...product.photos.split(","));
   }
+
+  console.log(product?.video_url);
 
   return (
     <div className="relative group max-w-[590px] block  bg-white rounded-3xl overflow-hidden">
@@ -30,7 +33,7 @@ const Card = ({ button, fav, product }: any) => {
         <div
           onClick={() => dispatch(addFavItem(product))}
           className={` ${
-            alreadyFav?.id ? 'bg-black text-white' : 'bg-white '
+            alreadyFav?.id ? "bg-black text-white" : "bg-white "
           } absolute top-5 left-5 w-10 h-10 border  flex items-center justify-center rounded-full cursor-pointer hover:text-white hover:bg-black duration-300 ease-in`}
         >
           <GrFavorite size={18} />
@@ -43,15 +46,27 @@ const Card = ({ button, fav, product }: any) => {
         onMouseLeave={() => setActive(false)}
         onMouseOverCapture={() => setActive(true)}
       >
-        {active && product?.video_url  ? (
+        {active && product?.video_url ? (
           <Link href={`/products/${product?.id}`} className=" h-full">
-            <VideoPlayer videoUrl={"https://www.youtube.com/embed/zuy6HQHOjvA?si=8cKkB66DOkHISdeq"} />
+            <ReactPlayer
+              width={`100%`}
+              height={`100%`}
+              muted
+              playing
+              loop
+              url={product?.video_url}
+              config={{
+                youtube: {
+                  playerVars: { showinfo: 0 },
+                },
+              }}
+            />
           </Link>
         ) : (
           <Link href={`/products/${product?.id}`} className=" h-full">
             <Image
               className=" w-full h-full "
-              src={process.env.BASE_URL + '/images/' + photos?.[0]}
+              src={process.env.BASE_URL + "/images/" + photos?.[0]}
               width={500}
               height={500}
               layout="responsive"
